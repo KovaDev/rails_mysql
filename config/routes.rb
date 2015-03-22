2606b0103cbcc12aa287f072679067b55bda4509
KovaDev <kova.dev@outlook.com>
2015-03-22 17:15:50 +0100

    Articles model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  root 'demo#index'
+
+  resources :articles
+
+  get 'demo/index'
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
